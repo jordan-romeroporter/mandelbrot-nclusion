827a4454cbd29748a2fe80a31774ba02c8ef080c
jordan-romeroporter <jordan.romeroporter@gmail.com>
2025-08-18 11:18:37 -0700

    chore: Fixed formatting inside types index file

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,22 +4,26 @@ export type Viewport = {
   zoom: number;
   width: number;
   height: number;
-}
+};
 
 export type ColorScheme = {
   name: string;
-  getColor: (isBounded: boolean, iterations: number, maxIterations: number) => RGB;
-}
+  getColor: (
+    isBounded: boolean,
+    iterations: number,
+    maxIterations: number
+  ) => RGB;
+};
 
 export type RGB = {
   r: number;
   g: number;
   b: number;
-}
+};
 
 export type MandelbrotPoint = {
   x: number;
   y: number;
   iterations: number;
   isBounded: boolean;
-}
+};
