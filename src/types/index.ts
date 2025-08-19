50acad99fcc07b1b45b5a58e76bbff45edd9f51b
jordan-romeroporter <jordan.romeroporter@gmail.com>
2025-08-19 11:27:05 -0700

    feat: update types with viewport and smooth coloring support

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,26 +4,30 @@ export type Viewport = {
   zoom: number;
   width: number;
   height: number;
-};
+}
 
 export type ColorScheme = {
   name: string;
-  getColor: (
-    isBounded: boolean,
-    iterations: number,
-    maxIterations: number
-  ) => RGB;
-};
+  getColor: (isBounded: boolean, iterations: number, maxIterations: number, smoothValue?: number) => RGB;
+}
 
 export type RGB = {
   r: number;
   g: number;
   b: number;
-};
+}
 
 export type MandelbrotPoint = {
   x: number;
   y: number;
   iterations: number;
   isBounded: boolean;
-};
+  smoothValue?: number;
+}
+
+export type ViewPreset = {
+  label: string;
+  centerX: number;
+  centerY: number;
+  zoom: number;
+}
